22b4250e5571e78c6400dafd6a49c9166cfaa647
Mark Ng <mng@tolerance.markng.co.uk>
2008-07-01 14:38:26 +0100

    first views

--- aggregator/views.py
+++ aggregator/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.http import HttpResponse
+from dotdorset.aggregator.models import *
+import pprint
+
+def index(request):
+  """index view"""
+  categories = Category.objects.all()
+  return HttpResponse(pprint.pformat(items))
--- urls.py
+++ urls.py
@@ -3,7 +3,7 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns('',
     # Example:
     # (r'^dotdorset/', include('dotdorset.foo.urls')),
-
+    (r'^feeds/$', 'dotdorset.aggregator.views.index'),
     # Uncomment this for admin:
     (r'^admin/', include('django.contrib.admin.urls')),
 )
