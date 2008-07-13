b8fb75992f446790cd0a9f882cc61b75b5ce7b2a
Mark Ng <mng@tolerance.markng.co.uk>
2008-07-13 09:11:24 +0100

    added in google maps keys for later on

--- aggregator/views.py
+++ aggregator/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from dotdorset.aggregator.models import *
+from dotdorset.settings import GOOGLE_MAPS_API_KEYS
 from django.template import Template
 from django.template.loader import render_to_string
 import pprint
@@ -13,6 +14,7 @@ def index(request):
   totemplate['jobitems'] = FeedItem.objects.filter(feed__category__name='Jobs').order_by('-pub_date')[0:2]
   totemplate['bookmarkitems'] = FeedItem.objects.filter(feed__category__name='Bookmarks').order_by('-pub_date')[0:20]
   totemplate['eventitems'] = FeedItem.objects.filter(feed__category__name='Events').order_by('-pub_date')[0:2]
-  
+  totemplate['map_api_key'] = GOOGLE_MAPS_API_KEYS[request.META['SERVER_NAME']]
+  totemplate['keys'] = GOOGLE_MAPS_API_KEYS
   rendered = render_to_string('aggregator_index.html', totemplate)
   return HttpResponse(rendered)
