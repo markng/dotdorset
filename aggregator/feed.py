6c71d71b2b81d5a7320c43cebc821069920efdff
Mark Ng <mng@tolerance.markng.co.uk>
2008-07-13 18:34:00 +0100

    adds RSS feeds, fixes #5

---
+++ aggregator/feed.py
@@ -0,0 +1,31 @@
+from django.contrib.syndication.feeds import Feed as DjangoFeed
+from django.contrib.syndication.feeds import FeedDoesNotExist
+from dotdorset.aggregator.models import FeedItem, Feed, Category
+
+class CategoryFeed(DjangoFeed):
+  """Category Feed"""
+  title = "dotdorset feed"
+  link = "/"
+  description = "test"
+  
+  def get_object(self, bits):
+    # In case of "/rss/beats/0613/foo/bar/baz/", or other such clutter,
+    # check that bits has only one member.
+    if len(bits) != 1:
+        raise ObjectDoesNotExist
+    return Category.objects.get(name__exact=bits[0])
+  
+  def items(self, obj):
+    """return items"""
+    return FeedItem.objects.filter(feed__category__id=obj.id).order_by('-pub_date')[:30]
+
+
+class FullFeed(DjangoFeed):
+  """Full Feed"""
+  title = "dotdorset feed"
+  link = "/"
+  description = "test"
+
+  def items(self):
+    """return items"""
+    return FeedItem.objects.order_by('-pub_date')[:30]
