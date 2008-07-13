1a8c5958289c67b5bd067954647b05cdd0a6aad6
Mark Ng <mng@tolerance.markng.co.uk>
2008-07-13 09:12:41 +0100

    maps js added (currently blank)

---
+++ js/maps.js
@@ -0,0 +1,7 @@
+/*
+function initialize() {
+  if (GBrowserIsCompatible()) {
+    var map = new GMap2(document.getElementById("gmap"));
+    map.setCenter(new GLatLng(50.792047,-2.340088), 10);
+  }
+}*/
