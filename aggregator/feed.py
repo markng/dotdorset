f28af51eea20e245924ada8a3811c98a0c416559
Mark Ng <mng@tolerance.markng.co.uk>
2008-07-14 09:29:43 +0100

    fixed up feeds

--- aggregator/feed.py
+++ aggregator/feed.py
@@ -4,8 +4,9 @@ from dotdorset.aggregator.models import FeedItem, Feed, Category
 
 class CategoryFeed(DjangoFeed):
   """Category Feed"""
-  title = "dotdorset feed"
   link = "http://dotdorset.org/"
+  title_template = 'feeds/all_title.html'
+  description_template = 'feeds/all_description.html'
   
   def get_object(self, bits):
     if len(bits) != 1:
@@ -15,10 +16,18 @@ class CategoryFeed(DjangoFeed):
   def description(self, obj):
     """description for feed"""
     return "%s from dotDorset" % obj
+
+  def title(self, obj):
+    """title for feed"""
+    return "%s from dotDorset" % obj
   
   def items(self, obj):
     """return items"""
     return FeedItem.objects.filter(feed__category__id=obj.id).order_by('-pub_date')[:30]
+  
+  def item_pubdate(self, item):
+    """get the right pubdate"""
+    return(item.pub_date)
 
 
 class FullFeed(DjangoFeed):
@@ -30,3 +39,7 @@ class FullFeed(DjangoFeed):
   def items(self):
     """return items"""
     return FeedItem.objects.order_by('-pub_date')[:30]
+  
+  def item_pubdate(self, item):
+    """get the right pubdate"""
+    return(item.pub_date)
