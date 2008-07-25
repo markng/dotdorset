17ece956ee09f7b724694dde56e09963d5284c90
Mark Ng <mng@tolerance.markng.co.uk>
2008-07-25 10:40:45 +0100

    tweak numbers for the page

--- aggregator/views.py
+++ aggregator/views.py
@@ -8,9 +8,9 @@ import pprint
 def index(request):
   """index view"""
   totemplate = {}
-  totemplate['blogitems'] = FeedItem.objects.filter(feed__category__name='Blogs').order_by('-pub_date')[0:5]
-  totemplate['twitteritems'] = FeedItem.objects.filter(feed__category__name='Twitter').order_by('-pub_date')[0:15]
-  totemplate['mlitems'] = FeedItem.objects.filter(feed__category__name='Mailing List').order_by('-pub_date')[0:5]
+  totemplate['blogitems'] = FeedItem.objects.filter(feed__category__name='Blogs').order_by('-pub_date')[0:7]
+  totemplate['twitteritems'] = FeedItem.objects.filter(feed__category__name='Twitter').order_by('-pub_date')[0:17]
+  totemplate['mlitems'] = FeedItem.objects.filter(feed__category__name='Mailing List').order_by('-pub_date')[0:6]
   totemplate['jobitems'] = FeedItem.objects.filter(feed__category__name='Jobs').order_by('-pub_date')[0:3]
   totemplate['bookmarkitems'] = FeedItem.objects.filter(feed__category__name='Bookmarks').order_by('-pub_date')[0:20]
   totemplate['eventitems'] = FeedItem.objects.filter(feed__category__name='Events').order_by('-pub_date')[0:3]
