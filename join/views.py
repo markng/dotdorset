a185ed79b8c338975b85c62e389d49489c6ad7f0
markng <mark@markng.co.uk>
2012-09-06 16:55:45 -0700

    RequestContext in there, so that staticfiles works.

--- join/views.py
+++ join/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 from django.template import Template
 from django.shortcuts import render_to_response
+from django.template import RequestContext
+
 
 def index(request):
   """index for join section"""
-  return render_to_response('join_index.html', {})
+  return render_to_response('join_index.html', RequestContext(request))
